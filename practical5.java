5a334c19a3142c5107cf98b8781f625689d8d019
Meet Darbar <darbarmeet007@gmail.com>
2025-04-20 23:35:35 +0530

    Update practical5.java

--- practical5.java
+++ practical5.java
@@ -3,60 +3,62 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
 
-
 public class practical5 extends Applet implements ItemListener {
 
-    List city, country, state;
+    List country, city, state;
+    String s, c, msg;
 
     public void init() {
         country = new List();
-        city = new List(4, true);
+        city = new List(3, true);
         state = new List();
 
-        city.add("khambhat");
-        city.add("Anand");
-        city.add("surat");
-        city.add("jaipur");
-        city.add("LA");
-        city.add("Patna");
-
-        state.add("Gujarat");
-        state.add("MP");
-        state.add("UP");
-        state.add("AP");
-        state.add("J&K");
-
-        country.add("Newzland");
-        country.add("Australia");
-        country.add("Pakistan");
+        country.add("INDIA");
+        country.add("PAKISTAN");
         country.add("USA");
-        country.add("Swterzerland");
+        country.add("JAPAN");
+        country.add("SOUTH KOREA");
+        country.add("CHINA");
 
-        add(country);
-        add(state);
-        add(city);
+        state.add("GUJARAT");
+        state.add("MADHPRADESH");
+        state.add("RAJASTHAN");
+        state.add("MAHARASHTRA");
+        state.add("KERLA");
+        state.add("AASAM");
+
+        city.add("AHEMEDABAD");
+        city.add("MUMBAI");
+        city.add("BANGOLURE");
+        city.add("DELHI");
+        city.add("KOLKATA");
 
         country.addItemListener(this);
         city.addItemListener(this);
         state.addItemListener(this);
 
+        add(country);
+        add(state);
+        add(city);
+
     }
 
-    public void paint(Graphics g) {
-        String s1, c1;
-        c1 = country.getSelectedItem();
-        s1 = state.getSelectedItem();
-        String city1[] = city.getSelectedItems();
-        String msg = " ";
-        for (int i = 0; i <= city1.length - 1; i++) {
-            msg = city1[i] + " " + msg;
-        }
-        g.drawString("country : " + c1, 50, 140);
-        g.drawString("State : " + s1, 50, 160);
-        g.drawString("city : " + msg, 50, 180);
-    }
     public void itemStateChanged(ItemEvent e) {
+        s = state.getSelectedItem();
+        c = country.getSelectedItem();
+
+        String ct[] = city.getSelectedItems();
+        msg = "  ";
+        for (int i = 0; i < ct.length; i++) {
+            msg = msg + "  " + ct[i];
+        }
         repaint();
     }
 
+    public void paint(Graphics g) {
+        g.drawString("Country  : " + c, 50, 100);
+        g.drawString("State    : " + s, 50, 120);
+        g.drawString("city     : " + msg, 50, 140);
+
+    }
 }
