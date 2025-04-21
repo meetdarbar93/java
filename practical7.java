42f94c296e0f360c62cecf7b0ad68afee040bd69
Meet Darbar <darbarmeet007@gmail.com>
2025-04-21 10:06:16 +0530

    Update practical7.java

--- practical7.java
+++ practical7.java
@@ -10,29 +10,38 @@ public class practical7 extends Applet implements ItemListener {
     Label l1;
 
     public void init() {
+        
         l1 = new Label("Select the color : ");
         c = new Choice();
+        
         c.add("red");
         c.add("blue");
         c.add("green");
         c.add("pink");
+        
         c.addItemListener(this);
         setLayout(null);
+        
         l1.setBounds(120, 20, 120, 20);
         c.setBounds(250, 20, 100, 30);
+        
         add(l1);
         add(c);
     }
 
     public void itemStateChanged(ItemEvent e){
+        
       str = c.getSelectedItem();
       repaint();
+        
     }
     public void paint(Graphics g){
-        if(str.equals("red")) setBackground(Color.orange);        
+        
+        if(str.equals("red")) setBackground(Color.red);        
         if(str.equals("blue")) setBackground(Color.blue);
         if(str.equals("green")) setBackground(Color.green);
         if(str.equals("pink")) setBackground(Color.pink);
 
     }
+    
 }
